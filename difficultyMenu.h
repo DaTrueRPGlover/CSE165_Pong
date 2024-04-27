285d61d7f23185d759b5a0e258382ba0e81bc48e
o0cheska0o <cfriend5@ucmerced.edu>
2024-04-27 09:21:10 -0700

    added controls intructions and fixed loop

--- difficultyMenu.h
+++ difficultyMenu.h
@@ -1,3 +1,6 @@
+
+
+
 class DifficultyMenu {
 public:
     DifficultyMenu(SDL_Renderer* renderer, const char* fontPath, int height, int width) : renderer(renderer) {
@@ -21,10 +24,9 @@ public:
     }
 
     void display(int& difficulty) {
-
         SDL_Color textColor = { 255, 255, 255 };
 
-       
+        // Render PONG title
         SDL_Surface* titleSurface = TTF_RenderText_Solid(titleFont, "PONG", textColor);
         SDL_Texture* titleTexture = SDL_CreateTextureFromSurface(renderer, titleSurface);
         SDL_Rect titleRect;
@@ -33,70 +35,78 @@ public:
         titleRect.x = (WINDOW_WIDTH - titleRect.w) / 2; // Center horizontally
         titleRect.y = WINDOW_HEIGHT / 4;
 
+        // Render difficulty level options
+        SDL_Surface* difficultySurface = TTF_RenderText_Solid_Wrapped(startGameFont, "PRESS NUMBER TO CHOOSE DIFFICULTY LEVEL\n1. Easy \n2. Medium \n3. Hard", textColor, 0);
+        SDL_Texture* difficultyTexture = SDL_CreateTextureFromSurface(renderer, difficultySurface);
+        SDL_Rect difficultyRect;
+        difficultyRect.w = difficultySurface->w;
+        difficultyRect.h = difficultySurface->h;
+        difficultyRect.x = (WINDOW_WIDTH - difficultyRect.w) / 2; // Center horizontally
+        difficultyRect.y = WINDOW_HEIGHT - difficultyRect.h - 50; // Position close to the bottom
 
+        // Render instructions for the player
+        SDL_Surface* instructionSurface = TTF_RenderText_Solid_Wrapped(startGameFont, "Player 1 (LEFT): Move up with 'w' and down with 's' \nPlayer 2 (RIGHT): Use the up and down arrow keys to move", textColor, 0);
+        SDL_Texture* instructionTexture = SDL_CreateTextureFromSurface(renderer, instructionSurface);
+        SDL_Rect instructionRect;
+        instructionRect.w = instructionSurface->w;
+        instructionRect.h = instructionSurface->h;
+        instructionRect.x = (WINDOW_WIDTH - instructionRect.w) / 2; // Center horizontally
+        instructionRect.y = (WINDOW_HEIGHT - instructionRect.h) / 2 + 50; // Center vertically
 
-        // Difficulty Level 
-        SDL_Surface* startSurface = TTF_RenderText_Solid_Wrapped(startGameFont, "PRESS NUMBER TO CHOOSE DIFFICULTY LEVEL\n1. Easy \n2. Medium \n3. Hard", textColor,0);
-        SDL_Texture* startTexture = SDL_CreateTextureFromSurface(renderer, startSurface);
-        SDL_Rect startRect;
-        startRect.w = startSurface->w;
-        startRect.h = startSurface->h;
-        startRect.x = (WINDOW_WIDTH - startRect.w) / 2; // Center horizontally
-        startRect.y = WINDOW_HEIGHT - startRect.h - 50; // Position close to the bottom
-
-
+        // Event loop for choosing difficulty level
         bool starting = true;
         SDL_Event event;
-        int pChoice = 0;
 
         while (starting) {
             while (SDL_PollEvent(&event)) {
                 if (event.type == SDL_QUIT) {
-                    SDL_FreeSurface(startSurface);
-                    SDL_DestroyTexture(startTexture);
-                    exit(0);
-                    break;
+                    starting = false;
                 }
                 else if (event.type == SDL_KEYDOWN) {
                     if (event.key.keysym.sym == SDLK_x) {
-                        SDL_FreeSurface(startSurface);
+                        SDL_FreeSurface(difficultySurface);
+                        SDL_DestroyTexture(difficultyTexture);
                         SDL_FreeSurface(titleSurface);
-                        SDL_DestroyTexture(startTexture);
                         SDL_DestroyTexture(titleTexture);
+                        SDL_FreeSurface(instructionSurface);
+                        SDL_DestroyTexture(instructionTexture);
                         exit(0);
                         break;
                     }
-
                     else if (event.key.keysym.sym == SDLK_1) {
-                       difficulty = 0;
-                        break;
-
+                        difficulty = 0;
+                        starting = false;
+                        //break;
                     }
                     else if (event.key.keysym.sym == SDLK_2) {
                         difficulty = 1;
-                        break;
+                        //break;
+                        starting = false;
                     }
-
                     else if (event.key.keysym.sym == SDLK_3) {
                         difficulty = 2;
-                        break;
+                        //break;
+                        starting = false;
                     }
-
                 }
             }
 
             SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
             SDL_RenderClear(renderer);
             SDL_RenderCopy(renderer, titleTexture, nullptr, &titleRect);
-            SDL_RenderCopy(renderer, startTexture, nullptr, &startRect);
+            SDL_RenderCopy(renderer, difficultyTexture, nullptr, &difficultyRect);
+            SDL_RenderCopy(renderer, instructionTexture, nullptr, &instructionRect);
             SDL_RenderPresent(renderer);
         }
-        SDL_FreeSurface(startSurface);
-        SDL_FreeSurface(titleSurface);
-        SDL_DestroyTexture(startTexture);
-        SDL_DestroyTexture(titleTexture);
 
+        SDL_FreeSurface(difficultySurface);
+        SDL_DestroyTexture(difficultyTexture);
+        SDL_FreeSurface(titleSurface);
+        SDL_DestroyTexture(titleTexture);
+        SDL_FreeSurface(instructionSurface);
+        SDL_DestroyTexture(instructionTexture);
     }
+
 
 private:
     SDL_Renderer* renderer;
